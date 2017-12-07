2fa4558af9414f6506de3b8853f46c706b858ce3
Sergio Mendoza <smendoza787@gmail.com>
2017-12-07 00:06:36 -0800

    rewrite CoinSelector component for redux

--- src/components/CoinSelector/index.js
+++ src/components/CoinSelector/index.js
@@ -1,31 +1,26 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { fetchCoins } from '../../actions/coinActions'
 import './index.css'
 
 class CoinSelector extends Component {
   constructor() {
     super()
 
-    this.state = {
-      coin: '',
-      coinObject: {},
-      coincapCoins: []
-    }
-
     this.handleChange = this.handleChange.bind(this)
-    // this.props.selectCoin = this.props.selecrCoin.bind(this)
+    // this.props.selectCoin = this.props.selectCoin.bind(this)
   }
 
   componentWillMount() {
-    fetch('https://api.coinmarketcap.com/v1/ticker/')
-      .then(resp => resp.json())
-      .then(coins => this.setState({ coincapCoins: coins }))
-
-    const selectedCoin = this.state.coincapCoins.find(coin => coin.symbol === this.state.coin)
-    this.setState({ coinObject: selectedCoin })
+    this.props.fetchCoins()
+    const selectedCoin = this.props.fetchedCoins.find(coin => coin.symbol === this.state.coin)
+    // TODO: Implement selectCoin action
+    // this.props.selectCoin(selectedCoin)
   }
 
   handleChange(event) {
-    const selectedCoin = this.state.coincapCoins.find(coin => coin.symbol === event.target.value)
+    const selectedCoin = this.props.fetchedCoins.find(coin => coin.symbol === event.target.value)
     this.setState({ 
       coin: event.target.value,
       coinObject: selectedCoin
@@ -50,7 +45,7 @@ class CoinSelector extends Component {
     return (
       <div className="coin-selector">
         <select value={this.state.coin} onChange={this.handleChange}>
-          {this.renderCoinOptions(this.state.coincapCoins)}
+          {this.renderCoinOptions(this.props.fetchedCoins)}
         </select>
         <h2>{this.state.coinObject && this.state.coinObject.symbol} (${this.state.coinObject && this.state.coinObject.price_usd})</h2>
       </div>
@@ -58,4 +53,18 @@ class CoinSelector extends Component {
   }
 }
 
-export default CoinSelector
+const mapStateToProps = (state) => {
+  return {
+    coinName: state.coin.coinName,
+    selectedCoin: state.coin.selectedCoin,
+    fetchedCoins: state.coin.fetchedCoins
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return bindActionCreators({
+    fetchCoins
+  }, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoinSelector)
