83a2da6a2567a2bf20681ae1cd5a300b80f9fbe6
Sergio Mendoza <smendoza787@gmail.com>
2017-12-07 23:20:39 -0800

    add CHANGE_USD_AMOUNT and CHANGE_FINAL_COIN_AMOUNT and more state variables to coinReducer

--- src/reducers/coinReducer.js
+++ src/reducers/coinReducer.js
@@ -1,4 +1,10 @@
-export default (state = { coinName: '', selectedCoin: {}, fetchedCoins: [] }, action) => {
+export default (state = {
+  coinName: '',
+  selectedCoin: {},
+  fetchedCoins: [],
+  usdPurchaseAmount: 0,
+  finalCoinAmount: 0
+}, action) => {
   switch (action.type) {
     case 'FETCH_COINS':
       return Object.assign({}, state, { fetchedCoins: action.payload })
@@ -6,6 +12,10 @@ export default (state = { coinName: '', selectedCoin: {}, fetchedCoins: [] }, ac
       return Object.assign({}, state, { selectedCoin: action.payload })
     case 'SET_COIN_NAME':
       return Object.assign({}, state, { coinName: action.payload })
+    case 'CHANGE_USD_AMOUNT':
+      return Object.assign({}, state, { usdPurchaseAmount: (action.payload) })
+    case 'CHANGE_FINAL_COIN_AMOUNT':
+      return Object.assign({}, state, { finalCoinAmount: action.payload })
     default:
       return state
   }
